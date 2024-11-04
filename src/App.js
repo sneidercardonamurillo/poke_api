1888827be3c33d95970c06c5f3dd09e5a2397438
Sneider Cardona <sneidercardonamurillo@gmail.com>
2024-11-04 13:31:52 -0500

    segundo_push

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import { useEffect } from 'react';
-import { Col, Spin } from 'antd';
-import { shallowEqual, useDispatch, useSelector } from 'react-redux';
-import Searcher from './components/Searcher';
-import PokemonList from './components/PokemonList';
-import logo from './statics/logo.svg';
-import { fetchPokemonsWithDetails } from './slices/dataSlice';
-import './App.css';
+import { useEffect } from "react";
+import { Col, Spin } from "antd";
+import { shallowEqual, useDispatch, useSelector } from "react-redux";
+import Searcher from "./components/Searcher";
+import PokemonList from "./components/PokemonList";
+import logo from "./statics/logo.svg";
+import { fetchPokemonsWithDetails } from "./slices/dataSlice";
+import "./App.css";
 
 function App() {
   const pokemons = useSelector((state) => state.data.pokemons, shallowEqual);
@@ -19,16 +19,16 @@ function App() {
   }, []);
 
   return (
-    <div className='App'>
+    <div className="App">
       <Col span={4} offset={10}>
-        <img src={logo} alt='Pokedux' />
+        <img src={logo} alt="Pokedux" />
       </Col>
       <Col span={8} offset={8}>
         <Searcher />
       </Col>
       {loading ? (
         <Col offset={12}>
-          <Spin spinning size='large' />
+          <Spin spinning size="large" />
         </Col>
       ) : (
         <PokemonList pokemons={pokemons} />
--- src/components/Searcher.jsx
+++ src/components/Searcher.jsx
@@ -1,7 +1,60 @@
-import { Input } from 'antd';
+import React, { useState } from "react";
+import { Input, List, message } from "antd";
 
 const Searcher = () => {
-  return <Input.Search placeholder='Buscar...' style={{ marginBottom: 10 }} />;
+  const [searchTerm, setSearchTerm] = useState("");
+  const [pokemonData, setPokemonData] = useState(null);
+
+  const handleSearch = async (value) => {
+    setSearchTerm(value);
+    if (value) {
+      try {
+        const response = await fetch(
+          `https://pokeapi.co/api/v2/pokemon/${value.toLowerCase()}`
+        );
+        if (!response.ok) {
+          throw new Error("Pokémon no encontrado");
+        }
+        const data = await response.json();
+        setPokemonData(data);
+      } catch (error) {
+        message.error(error.message);
+        setPokemonData(null);
+      }
+    }
+  };
+
+  return (
+    <div>
+      <Input.Search
+        placeholder="Buscar Pokémon..."
+        style={{ marginBottom: 10 }}
+        onSearch={handleSearch}
+        enterButton
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      {pokemonData && (
+        <List
+          header={<div>Resultado de búsqueda:</div>}
+          bordered
+          dataSource={[pokemonData]}
+          renderItem={(item) => (
+            <List.Item>
+              <div>
+                <strong>{item.name}</strong>
+                <img
+                  src={item.sprites.front_default}
+                  alt={item.name}
+                  style={{ marginLeft: 10 }}
+                />
+              </div>
+            </List.Item>
+          )}
+        />
+      )}
+    </div>
+  );
 };
 
 export default Searcher;

ebbe290aae2a238d3e032c7a269c66db8c423127
Sneider Cardona <sneidercardonamurillo@gmail.com>
2024-11-04 17:47:31 -0500

    cambio

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,27 +1,42 @@
-import { Card } from 'antd';
-import { useDispatch } from 'react-redux';
-import Meta from 'antd/lib/card/Meta';
-import StarButton from './StarButton';
-import { setFavorite } from '../slices/dataSlice';
-import './PokemonList.css';
+import { Card, Tooltip } from "antd";
+import { useDispatch } from "react-redux";
+import Meta from "antd/lib/card/Meta";
+import StarButton from "./StarButton";
+import { setFavorite } from "../slices/dataSlice";
+import PropTypes from "prop-types";
+import "./PokemonList.css";
 
 const PokemonCard = ({ name, image, types, id, favorite }) => {
   const dispatch = useDispatch();
-  const typesString = types.map((elem) => elem.type.name).join(', ');
+  const typesString = types.map((elem) => elem.type.name).join(", ");
 
   const handleOnFavorite = () => {
     dispatch(setFavorite({ pokemonId: id }));
   };
 
   return (
-    <Card
-      title={name}
-      cover={<img src={image} alt={name} />}
-      extra={<StarButton isFavorite={favorite} onClick={handleOnFavorite} />}
-    >
-      <Meta description={typesString} />
-    </Card>
+    <div className="card-container">
+      {" "}
+      {/* Contenedor para centrar el card */}
+      <Card
+        className="rounded-card" // Clase CSS para bordes redondeados
+        title={<Tooltip title={typesString}>{name}</Tooltip>}
+        cover={image ? <img src={image} alt={name} /> : <div>No Image</div>}
+        extra={<StarButton isFavorite={favorite} onClick={handleOnFavorite} />}
+      >
+        <Meta description={typesString} />
+      </Card>
+    </div>
   );
+};
+
+// Definición de tipos de propiedades
+PokemonCard.propTypes = {
+  name: PropTypes.string.isRequired,
+  image: PropTypes.string,
+  types: PropTypes.arrayOf(PropTypes.object).isRequired,
+  id: PropTypes.number.isRequired,
+  favorite: PropTypes.bool.isRequired,
 };
 
 export default PokemonCard;
--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -1,21 +1,20 @@
-import React from "react";
 import PokemonCard from "./PokemonCard";
-import "./PokemonList.css";
 
 const PokemonList = ({ pokemons }) => {
   return (
     <div className="PokemonList">
-      {pokemons.map((pokemon) => (
-        <div className="PokemonCard" key={pokemon.name}>
+      {pokemons.map((pokemon) => {
+        return (
           <PokemonCard
             name={pokemon.name}
+            key={pokemon.name}
             image={pokemon.sprites.front_default}
             types={pokemon.types}
             id={pokemon.id}
             favorite={pokemon.favorite}
           />
-        </div>
-      ))}
+        );
+      })}
     </div>
   );
 };
